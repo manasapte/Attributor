6508678e445712bc6cb87a2ac7ea1632bcc6a70a
Manas Apte <manasapte@gmail.com>
2014-05-31 17:43:28 -0700

    committing all the changes

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -9,7 +9,9 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 @end
 
 @implementation AttributorViewController
@@ -18,6 +20,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString* title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes: @{ NSStrokeWidthAttributeName: @3, NSStrokeColorAttributeName: self.outlineButton.tintColor}
+                   range:NSMakeRange(0, title.length)];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+}
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+- (IBAction)outlineBodySelection:(id)sender {
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName: @-3, NSStrokeColorAttributeName: [UIColor blackColor]}
+                                   range: self.body.selectedRange];
+}
+- (IBAction)unoutlineBodySelection:(id)sender {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
 }
 
 - (void)didReceiveMemoryWarning
